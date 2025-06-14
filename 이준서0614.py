0ed3f2c72581f78b3ce92714bd11a7b6d4d20443
junseo387 <ljjs0506@gmail.com>
2025-06-14 16:47:35 +0900

    Add files via upload

---
+++ 이준서0614.py
@@ -0,0 +1,33 @@
+##a, b, c = map(int, input().split())
+##if a > 
+
+##n = int(input())
+##a = 0
+##for i in  range(1, 1+n):
+##  if i%2==1:
+##    a+=i
+##print(a)
+
+##n = int(input())
+##a = 0
+##for i in range(1, 1+n):
+##  if i%2==0:
+##    a+=1
+##print(a)
+
+##n = int(input())
+##a = 0
+##for i in range(1, 1+n):
+##  if n%i==0:
+##    print(i)
+
+##n = int(input())
+##for i in range(1, 1+n):
+##  if (i%3==0):
+##    print(i)
+##  elif (i%2==0):
+##    print(i)
+
+n = int(input())
+for i in range(1, 1+n):
+  if (
